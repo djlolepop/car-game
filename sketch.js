bf43a1edc1343ea4388a18453c869e2f61c22558
djlolepop <67583626+djlolepop@users.noreply.github.com>
2020-09-02 18:00:09 +0530

    car game
    car game

--- sketch.js
+++ sketch.js
@@ -9,7 +9,14 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var car1img,car2img,car3img,car4img;
+function preload(){
+  car1img = loadImage("images/car1.png");
+  car2img = loadImage("images/car2.png");
+  car3img = loadImage("images/car3.png");
+  car4img = loadImage("images/car4.png");
+  trackimg = loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
